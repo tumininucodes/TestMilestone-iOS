c48ab6fdc8e7f2a2a2a323047dec7b2f12791331
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2025-12-03 04:26:46 +0100

    Initial Commit

---
+++ TestMilestone/TestMilestoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMilestoneApp.swift
+//  TestMilestone
+//
+//  Created by Oluwatumininu Ojo on 03.12.25.
+//
+
+import SwiftUI
+
+@main
+struct TestMilestoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
